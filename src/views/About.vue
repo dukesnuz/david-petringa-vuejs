0fa159a59dfb58cee496b0ea48e672393543720a
David Petringa <david@ajaxtransport.com>
2019-11-13 17:31:41 -0600

    added meta elements and about content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Greetings</h2>
+    <p>
+      This is a simple website made with Vue.js. My goal in developing this site is to practice and become more
+      proficient with Vue.js. Vue.js is a frontend web develpment framework. I discovered and started using Vue.js
+      when I was learning the PHP Framework, Laravel. I coded <a href="https://david.dukesnuz.com/links/get-list" target="blank">this</a>
+      using Laravel. Vue.js was not used.
+      I am using the following technologies for this site.
+      </p>
+      <ul>
+        <li><a href="https://code.visualstudio.com/" target="blank">VS Code</a> for a text editor</li>
+        <li><a href="https://github.com/dukesnuz/david-petringa-vuejs" target="blank">Github</a> for version control</li>
+        <li><a href="https://m.do.co/c/3ec7cdf44173" target="blank">Digital Ocean</a> for hosting</li>
+      </ul>
+
+      
   </div>
 </template>
+
+<style scoped>
+.about {
+  margin-bottom: 500px;
+  padding: 0 25% 0 25%;
+}
+ul {
+background:aliceblue;
+padding: 5px;
+width: 250px;
+margin: auto;
+}
+</style>
